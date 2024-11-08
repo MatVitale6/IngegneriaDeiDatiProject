eac03f579b514e38c56c99781ba4b016c6b04110
teresavaillant <ter.vaillant@stud.uniroma3.it>
2024-11-08 11:34:10 +0100

    modifiche della classe SearchResult

--- homework2/lucenehw/src/main/java/it/uniroma3/ingegneriadeidati/llmagent/lucenehw/model/SearchResult.java
+++ homework2/lucenehw/src/main/java/it/uniroma3/ingegneriadeidati/llmagent/lucenehw/model/SearchResult.java
@@ -5,6 +5,44 @@ public class SearchResult {
     private String author;
     private String contentSnippet;
 
-    //TODO: write Constructors, Getters and Setters
+    // TODO: write Constructors, Getters and Setters
+    
+    // Costruttore senza argomenti
+    public SearchResult() {
+    }
+
+    // Costruttore con tutti i campi
+    public SearchResult(String title, String author, String contentSnippet) {
+        this.title = title;
+        this.author = author;
+        this.contentSnippet = contentSnippet;
+    }
+
+    // Getter e Setter per 'title'
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter e Setter per 'author'
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Getter e Setter per 'contentSnippet'
+    public String getContentSnippet() {
+        return contentSnippet;
+    }
+
+    public void setContentSnippet(String contentSnippet) {
+        this.contentSnippet = contentSnippet;
+    }
 
 }
